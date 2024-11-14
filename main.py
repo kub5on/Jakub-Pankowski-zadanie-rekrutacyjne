f3f86e5dc68ed6c2ab32b51cd5b1755f9eec7944
kub5on <jaakub.pankowski@gmail.com>
2024-11-14 19:17:34 +0100

    creating new function - processing_result

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from openai import OpenAI
 import openai
 import os
 
+
 def api_key_request():
 
     while True:
@@ -23,6 +24,7 @@ def api_key_request():
             OPENAI_API_KEY = input(str("Twój klucz API nie działa bądź nie istnieje, podaj swój klucz API OpenAI: "))
             os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
+
 def ask_openai(system_content, user_prompt, article):
 
     client = OpenAI()
@@ -33,17 +35,34 @@ def ask_openai(system_content, user_prompt, article):
             {"role": "user", "content": user_prompt + article}
         ]
     )
-    answer = completion.choices[0].message
+    # answer = completion.choices[0].message['content']
     # print(completion.choices[0].message)
-    return answer
+    # return answer
+    result = completion.choices[0].message.content  # Pobiera tekst odpowiedzi
+    return result
 
 
 def accept_arguments(name, value):
     new_value = input(f"{name}{value} --> ")
     return new_value if new_value else value
 
-# api_key_request()
 
+def processing_result(result):
+
+    start_index = result.find("<!DOCTYPE html>")
+    if start_index != -1:
+        result = result[start_index:]
+
+    stop_index = result.find("</html>")
+    if stop_index != -1:
+        result = result[:stop_index + len("</html>")]
+
+    # Zapisanie wygenerowanego HTML do pliku
+    with open("artykul.html", "w", encoding="UTF-8") as output_file:
+        output_file.write(result)
+
+
+api_key_request()
 
 # Wczytanie treści artykułu
 with open('artykul.txt', 'r', encoding='UTF-8') as file:
@@ -65,11 +84,9 @@ print("Dane które zostaną przesłane do OpenAi:\n"
       "\nZaakceptuj klikając enter bądź ustaw nowe wartości dla: \n ")
 
 # akceptacja danych gotowych do przekazania do OpenAI
-
 user_prompt = accept_arguments("Wiadomość użytkownika (user_prompt): ", user_prompt)
 system_content = accept_arguments("\nInstrukcja dla systemu (system_content): ", system_content)
 
+answer = ask_openai(system_content, user_prompt, article)
 
-print(user_prompt)
-
-# ask_openai(system_content, user_prompt, article)
+processing_result(answer)
