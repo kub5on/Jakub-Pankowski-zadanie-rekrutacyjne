795a4ea8b82429659025dc10a8c94fcd105b47ab
kub5on <jaakub.pankowski@gmail.com>
2024-11-14 16:03:27 +0100

    Creating function api_key_request, which ask user for API key and then check its validity

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 from openai import OpenAI
+import openai
+import os
+
+
+def api_key_request():
+    OPENAI_API_KEY = input(str("Podaj swój klucz API OpenAI: "))
+    os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+    while True:
+        try:
+            client = OpenAI()
+            completion = client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=[
+                    {"role": "user", "content": "Hello world"}
+                ]
+            )
+            print("Klucz poprawny!")
+            break
+        except openai.AuthenticationError:
+            OPENAI_API_KEY = input(str("Klucz niepoprawny, podaj swój klucz API OpenAI: "))
+            os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
 
 
 def ask_openai(system_content, prompt):
@@ -13,4 +36,5 @@ def ask_openai(system_content, prompt):
     print(completion.choices[0].message)
 
 
-ask_openai("", "Przywitaj się.")
+# ask_openai("", "Przywitaj się.")
+api_key_request()
