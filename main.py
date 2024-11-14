7bcaecf012eaacfecb81f3646223a3da698444c7
kub5on <jaakub.pankowski@gmail.com>
2024-11-14 18:53:03 +0100

    creating new function -  accept_arguments, user can accept default values or change them (values of variables which will be send to openai)

--- main.py
+++ main.py
@@ -38,14 +38,38 @@ def ask_openai(system_content, user_prompt, article):
     return answer
 
 
+def accept_arguments(name, value):
+    new_value = input(f"{name}{value} --> ")
+    return new_value if new_value else value
+
 # api_key_request()
+
 
 # Wczytanie treści artykułu
 with open('artykul.txt', 'r', encoding='UTF-8') as file:
     article = file.read()
 
+system_content = "\nJesteś programistą HTML, twoją pracą jest odpowiednie obrabianie tekstu do wstawienia na stronę internetową."
+
+user_prompt = "\nStwórz kod HTML z artykułem z odpowiednimi tagami HTML do strukturyzacji treści, który zawiera miejsca na obrazy. \n" \
+              "Wstaw tagi <img src='image_placeholder.jpg' alt='...' /> tam, gdzie artykuł może wymagać obrazu, \n" \
+              "wypełnij atrybut 'alt' odpowiednim opisem dla grafiki, który można wykorzystać do jej wygenerowania.\n" \
+              "Dodaj podpisy pod grafikami z użyciem tagu <figcaption>."
+                # "Nie generuj kodu CSS ani JavaScript. Kod HTML ma być tylko między tagami <body> i </body>."
 
 
+print("Dane które zostaną przesłane do OpenAi:\n"
+      "1. Treść artykułu (article),\n"
+      "2. Wiadomość użytkownika (user_prompt),\n"
+      "3. Instrukcja dla systemu (system_content)\n"
+      "\nZaakceptuj klikając enter bądź ustaw nowe wartości dla: \n ")
 
-#
+# akceptacja danych gotowych do przekazania do OpenAI
+
+user_prompt = accept_arguments("Wiadomość użytkownika (user_prompt): ", user_prompt)
+system_content = accept_arguments("\nInstrukcja dla systemu (system_content): ", system_content)
+
+
+print(user_prompt)
+
 # ask_openai(system_content, user_prompt, article)
